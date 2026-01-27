b665a4ef327467fcddd7444bee5c67003bf654c8
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-27 14:56:24 +0300

    Init commit.

---
+++ src/test/java/ru/kuznetsov/shop/dataaccesfeign/AccesFeignApplicationTests.java
@@ -0,0 +1,13 @@
+//package ru.kuznetsov.shop.dataaccesfeign;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AccesFeignApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
